1ccaa5e30b325da2c4e1d8906d49e0843fa8f24b
NAKATA Maho <maho.nakata@gmail.com>
2023-07-26 13:56:41 +0900

    .

--- sdpa_linear.cpp
+++ sdpa_linear.cpp
@@ -87,7 +87,7 @@ mpf_class Lal::getMinEigen(DenseMatrix& lMat,
       // rMessage("b = ");
       // b.display();
 
-      mpackint info;
+      mplapackint info;
       int kp1 = k+1;
       Rsteqr ("I_withEigenvalues", kp1, out.ele, b.ele, Q.de_ele, Q.nRow, workVec.ele, &info);
 
@@ -145,8 +145,8 @@ mpf_class Lal::getMinEigenValue(DenseMatrix& aMat,
   // and needs memory of length aMat.nRow 
   // workVec is temporary space and needs
   // 3*aMat.nRow-1 length memory.
-  mpackint N = aMat.nRow;
-  mpackint LWORK, info;
+  mplapackint N = aMat.nRow;
+  mplapackint LWORK, info;
   switch (aMat.type) {
   case DenseMatrix::DENSE:
     LWORK = 3*N-1;
@@ -339,7 +339,7 @@ bool Lal::getCholesky(DenseMatrix& retMat,DenseMatrix& aMat)
     rError("getCholesky:: different memory size");
   }
   int length,shou,amari;
-  mpackint info; 
+  mplapackint info; 
   switch (retMat.type) {
   case DenseMatrix::DENSE:
     length = retMat.nRow * retMat.nCol;
@@ -623,7 +623,7 @@ int Lal::rdpotrf_(char *uplo, int *n, double *a, int *lda, int *info)
 
 bool Lal::choleskyFactorWithAdjust(DenseMatrix& aMat)
 {
-  mpackint info=0;
+  mplapackint info=0;
 #if 1
   // aMat.display();
   TimeStart(START1);
